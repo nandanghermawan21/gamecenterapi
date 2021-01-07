eb7262b3b27ee495a5b4f93e439f2b9b78047d23
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-01-08 04:51:04 +0700

    commit

--- application/models/M_main.php
+++ application/models/M_main.php
@@ -3,8 +3,8 @@
 class M_main extends CI_Model
 {
 
-    function get_user($q)
-    {
-        return $this->db->get_where('m_user', $q);
-    }
+	function get_user($q)
+	{
+		return $this->db->get_where('m_user', $q);
+	}
 }
