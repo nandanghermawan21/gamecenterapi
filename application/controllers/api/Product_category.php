80bf9c97b1fa9500118d3390cefe73ed30f2558c
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-01-09 22:22:47 +0700

    commit

--- application/controllers/api/Product_category.php
+++ application/controllers/api/Product_category.php
@@ -20,9 +20,8 @@ class Main extends BD_Controller
      *   @OA\Response(response=200,
      *     description="repositories owned by the supplied user",
      *     @OA\JsonContent(type="array",
-     *       @OA\Items(ref="#/components/schemas/repository")
+     *       @OA\Items(ref="#/components/schemas/category")
      *     ),
-     *     @OA\Link(link="userRepository", ref="#/components/links/UserRepository")
      *   )
      * @OA\Response(response="404", description="not found")
      * )
