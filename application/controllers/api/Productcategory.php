7a90b2795482b13e1e744a9b79bce68efaf324d8
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-01-09 23:08:34 +0700

    commit

--- application/controllers/api/Productcategory.php
+++ application/controllers/api/Productcategory.php
@@ -4,6 +4,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * @OA\Info(title="Game Center API", version="0.1")
+ * @OA\SecurityScheme(
+ *   securityScheme="token",
+ *   type="apiKey",
+ *   name="Authorization",
+ *   in="header"
+ * )
  */
 class Productcategory extends BD_Controller
 {
