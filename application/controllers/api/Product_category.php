9a37be63982f42ae79f6c1f5e1d98e7b2efa2a7b
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-01-09 22:37:08 +0700

    test

--- application/controllers/api/Product_category.php
+++ application/controllers/api/Product_category.php
@@ -16,14 +16,25 @@ class Main extends BD_Controller
     }
 
     /**
-     * @OA\Get(path="/api/category/all",tags={"Category"},
+     * @OA\Get(path="/2.0/repositories/{username}",
+     *   operationId="getRepositoriesByOwner",
+     *   @OA\Parameter(
+     *     name="username",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(type="string")
+     *   ),
      *   @OA\Response(response=200,
      *     description="repositories owned by the supplied user",
      *     @OA\JsonContent(type="array",
-     *       @OA\Items(ref="#/schemas/category")
+     *       @OA\Items(ref="#/components/schemas/category")
      *     ),
+     *     @OA\Link(link="userRepository", ref="#/components/links/UserRepository")
      *   )
-     * @OA\Response(response="404", description="not found")
+     * )
+     * @OA\Link(link="UserRepositories",
+     *   operationId="getRepositoriesByOwner",
+     *   parameters={"username"="$response.body#/username"}
      * )
      */
     public function all_get()
