c65a05c1e951678d0f90e2dedb59e7926f4364a4
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-01-09 21:31:27 +0700

    commit

--- application/controllers/api/Product_category.php
+++ application/controllers/api/Product_category.php
@@ -16,14 +16,9 @@ class Main extends BD_Controller
     }
 
     /**
-     * @OA\get(path="/api/category/all",tags={"Auth"},
-     *   @OA\Response(response=200,
-     *     description="All Product Category",
-     *     @OA\JsonContent(type="array",
-     *       @OA\Items(ref="#/components/schemas/category")
-     *     ),
-     *     @OA\Link(link="userRepository", ref="#/components/links/UserRepository")
-     *   )
+     * @OA\Post(path="/api/auth/login",tags={"Auth"},
+     * @OA\Response(response="200", description="An example resource"),
+     * @OA\Response(response="404", description="not found")
      * )
      */
     public function all_get()
