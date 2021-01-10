efcae2ed270a672e285e156919cb586610f20c95
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-01-10 09:14:57 +0700

    commit

--- application/core/BD_Controller.php
+++ application/core/BD_Controller.php
@@ -1,4 +1,4 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 /** @noinspection PhpIncludeInspection */
@@ -7,11 +7,12 @@ require_once APPPATH . '/libraries/JWT.php';
 require_once APPPATH . '/libraries/BeforeValidException.php';
 require_once APPPATH . '/libraries/ExpiredException.php';
 require_once APPPATH . '/libraries/SignatureInvalidException.php';
+
 use \Firebase\JWT\JWT;
 
 class BD_Controller extends REST_Controller
 {
-	private $user_credential;
+    private $user_credential;
     public function auth()
     {
         // Configure limits on our controller methods
@@ -22,18 +23,18 @@ class BD_Controller extends REST_Controller
         //JWT Auth middleware
         $headers = $this->input->get_request_header('Authorization');
         $kunci = $this->config->item('thekey'); //secret key for encode and decode
-        $token= "token";
-       	if (!empty($headers)) {
-        	if (preg_match('/Bearer\s(\S+)/', $headers , $matches)) {
-            $token = $matches[1];
-        	}
-    	}
+        $token = "token";
+        if (!empty($headers)) {
+            if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+                $token = $matches[1];
+            }
+        }
         try {
-           $decoded = JWT::decode($token, $kunci, array('HS256'));
-           $this->user_data = $decoded;
+            $decoded = JWT::decode("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjIiLCJ1c2VybmFtZSI6ImRvZGkiLCJpYXQiOjE2MTAyNDQ0NDMsImV4cCI6MTYxMDI2MjQ0M30.oSScc4zKtiP_dWYatUSSv1zR7YXP3h67fACDJFOdc8o", $kunci, array('HS256'));
+            $this->user_data = $decoded;
         } catch (Exception $e) {
             $invalid = ['status' => $e->getMessage()]; //Respon if credential invalid
-            $this->response($invalid, 401);//401
+            $this->response($invalid, 401); //401
         }
     }
 }
