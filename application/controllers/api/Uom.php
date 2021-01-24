0565b967373bc977b23d35b2850b965b6fa10aef
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-01-24 23:22:17 +0700

    commit

---
+++ application/controllers/api/Uom.php
@@ -0,0 +1,120 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * @OA\Info(title="Game Center API", version="0.1")
+ * @OA\SecurityScheme(
+ *   securityScheme="token",
+ *   type="apiKey",
+ *   name="Authorization",
+ *   in="header"
+ * )
+ */
+class Uom extends BD_Controller
+{
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->auth();
+        $this->load->helper('string');
+        $this->load->model("m_uom", "uom");
+        $this->load->model("m_uomConversion", "uomConversion");
+        $this->load->model("m_uomConversionTree", "uomConversionTree");
+        $this->load->model('errormodel', 'errormodel');
+    }
+
+    /**
+     * @OA\Get(path="/api/uom/all",tags={"uom"},
+     *   operationId="getAllUom",
+     *   @OA\Response(response=200,
+     *     description="UOM Master",
+     *     @OA\JsonContent(type="array",
+     *       @OA\Items(ref="#/components/schemas/uom")
+     *     ),
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
+    public function all_get()
+    {
+        $data = $this->uom->getAll();
+        $this->response($data, 200); // OK (200) being the HTTP response code
+    }
+
+    /**
+     * @OA\Post(path="/api/uom/add",tags={"uom"},
+     *   operationId="add Uom",
+     *   @OA\RequestBody(
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/uom")
+     *     )
+     *   ),
+     *   @OA\Response(response=200,
+     *     description="categpry product",
+     *     @OA\JsonContent(
+     *       @OA\Items(ref="#/components/schemas/uom")
+     *     ),
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
+    public function add_post()
+    {
+        try {
+            $jsonBody  = json_decode(file_get_contents('php://input'), true);
+            $uom = $this->uom->fromJson($jsonBody);
+
+            if ($uom->uomId == "" || $uom->uomId == null) {
+                $uom->uomId = random_string('num', 10);
+            }
+
+            $result = $this->category->add($uom);
+
+            $this->response($result, 200);
+        } catch (\Exception $e) {
+            $error = new errormodel();
+            $error->status = 500;
+            $error->message = $e->getMessage();
+
+            $this->response($error, 500);
+        }
+    }
+
+    /**
+     * @OA\Post(path="/api/uom/update",tags={"uom"},
+     *   operationId="update uom",
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="query",
+     *     required=true,
+     *     @OA\Schema(type="string")
+     *   ),
+     *   @OA\RequestBody(
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/uom")
+     *     ),
+     *   ),
+     *   @OA\Response(response=200,
+     *     description="categpry product",
+     *     @OA\JsonContent(
+     *       @OA\Items(ref="#/components/schemas/uom")
+     *     ),
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
+    public function update_post()
+    {
+        $id = $this->input->get('id', TRUE);
+        $jsonBody  = json_decode(file_get_contents('php://input'), true);
+        $category = $this->category->fromJson($jsonBody);
+
+        $result = $this->uom->update($id, $category);
+
+        $this->response($result, 200);
+    }
+}
---
+++ application/models/M_uom.php
@@ -0,0 +1,98 @@
+<?php if (!defined('BASEPATH')) exit('No direct script allowed');
+
+/**
+ * @OA\Schema(schema="uom")
+ */
+class M_uom extends CI_Model
+{
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $uomId;
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $uomName;
+
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $uomCode;
+
+    function fromRow($row)
+    {
+        $data = new M_uom();
+        $data->uomId = $row->uom_id;
+        $data->uomName = $row->uom_name;
+        $data->uomCode = $row->uom_code;
+        return $data;
+    }
+
+    function fromJson($json)
+    {
+        $data = new M_uom();
+
+        if (isset($json['uomId'])) {
+            $data->uomId = $json["uomId"];
+        }
+        if (isset($json['uomName'])) {
+            $data->uomName = $json["uomName"];
+        }
+        if (isset($json['uomCode'])) {
+            $data->uomCode = $json["uomCode"];
+        }
+
+        return $data;
+    }
+
+    function toRow()
+    {
+        $data = new stdClass();
+        $data->uom_id = $this->uomId;
+        $data->uom_name = $this->uomName;
+        $data->uom_code = $this->uomCode;
+
+        return $data;
+    }
+
+
+    function getAll()
+    {
+        $query = $this->db->get('m_uom');
+
+        $result = [];
+        foreach ($query->result() as $row) {
+            array_push($result, $this->fromRow($row));
+        }
+
+        return $result;
+    }
+
+    function add($uom)
+    {
+        $this->db->insert('m_uom', $uom->toRow());
+
+        $data = $this->db->get_where('m_category', array('id' => $uom->id));
+
+        return $this->fromRow($data->result()[0]);
+    }
+
+    function update($id, \M_uom $uom)
+    {
+        $data = array(
+            'uom_name' => $uom->uomName,
+            'uom_code' => $uom->uomCode,
+        );
+
+        $this->db->where('id', $id);
+        $this->db->update('m_uom', $data);
+
+        $data = $this->db->get_where('m_uom', array('id' => $id));
+
+        return $this->fromRow($data->result()[0]);
+    }
+}
