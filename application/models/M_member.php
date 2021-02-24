f49c5c667ffe4d9a6f13a11f1b94d051d40e8b3f
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-02-25 06:23:25 +0700

    commmit

--- application/models/M_member.php
+++ application/models/M_member.php
@@ -257,7 +257,7 @@ class M_member extends CI_Model
 
 			$data = $this->db->get_where($this->tableName(), array('id' => $this->id));
 
-			return $this->fromRow($data->result()[0]);
+			return $this->fromRow($data[0]);
 		} catch (Exception $e) {
 			throw $e;
 		}
