67987c3017a8bad08194cbd1c16d1a48149fd7a6
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-05-15 18:58:07 +0700

    module voucher rev 1 test 5

--- db/query/m_voucher.sql
+++ db/query/m_voucher.sql
@@ -12,13 +12,6 @@ create table m_voucher(
     create_date datetime
 )
 
-create table trx_voucher(
-    noref varchar(50) primary,
-    member_id varchar(12),
-    voucher_id varchar(12),
-    used_date datetime,
-)
-
 select * from m_voucher
 where id = '283725440099'
 
@@ -27,6 +20,6 @@ set point = 5 where point is null
 
 delete from m_voucher
 
-drop table m_voucher
+drop table trx_voucher
 
 select * from m_member
