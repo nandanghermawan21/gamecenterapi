d0442233f64d5bab0ba117c3add90f1d2871328e
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-06-13 15:26:44 +0700

    add transactiin type module v1

---
+++ application/controllers/api/TrxType.php
@@ -0,0 +1,112 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * @OA\Info(title="Game Center API", version="0.1")
+ * @OA\SecurityScheme(
+ *   securityScheme="token",
+ *   type="apiKey",
+ *   name="Authorization",
+ *   in="header"
+ * )
+ */
+class TrxType extends BD_Controller{
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->auth();
+        $this->load->helper('string');
+        $this->load->model("m_trxType", "trxType");
+        $this->load->model('errormodel', 'errormodel');
+    }
+
+      /**
+     * @OA\Post(path="/api/trxType/add",tags={"TransactionType"},
+     *   operationId="add trxType",
+     *   @OA\RequestBody(
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/trxType")
+     *     )
+     *   ),
+     *   @OA\Response(response=200,
+     *     description="add trxType",
+     *     @OA\JsonContent(
+     *       @OA\Items(ref="#/components/schemas/trxType")
+     *     ),
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
+    public function add_post()
+    {
+        if ($this->user_data->type == "admin") {
+            try {
+                $jsonBody  = json_decode(file_get_contents('php://input'), true);
+                $trxType = $this->trxType->fromJson($jsonBody);
+
+                if ($trxType->validateCode() == true) {
+                    $this->response("Code As Exist", 400);
+                } else {
+                    $result = $this->trxType->fromJson($jsonBody)->add();
+                    $this->response($result, 200);
+                }
+            } catch (\Exception $e) {
+                $error = new errormodel();
+                $error->status = 500;
+                $error->message = $e->getMessage();
+                $this->response($error, 500);
+            }
+        } else {
+            $this->response("Access Denied", 500);
+        }
+    }
+
+    /**
+     * @OA\Get(path="/api/member/trxType",tags={"TransactionType"},
+     *   operationId="get trxType",
+     *   @OA\Parameter(
+     *       name="code",
+     *       in="query",
+     *       required=false,
+     *       @OA\Schema(type="string")
+     *   ),
+     *   @OA\Parameter(
+     *       name="searchKey",
+     *       in="query",
+     *       required=false,
+     *       @OA\Schema(type="string")
+     *   ),
+     *   @OA\Parameter(
+     *       name="skip",
+     *       in="query",
+     *       required=false,
+     *       @OA\Schema(type="int")
+     *   ),
+     *   @OA\Parameter(
+     *       name="limit",
+     *       in="query",
+     *       required=false,
+     *       @OA\Schema(type="int")
+     *   ),
+     *   @OA\Response(response=200,
+     *     description="get member",
+     *     @OA\JsonContent(type="array",
+     *       @OA\Items(ref="#/components/schemas/trxType")
+     *     ),
+     *   ),
+     *   security={{"token": {}}},
+     * )
+     */
+    public function get_get()
+    {
+        $code = $this->get("code", true);
+        $serchKey = $this->get("searchKey", true);
+        $limit = $this->get("limit", true);
+        $skip = $this->get("skip", true);
+        $data = $this->member->get($code, $serchKey, $limit, $skip);
+        $this->response($data, 200); // OK (200) being the HTTP response code
+    }
+}
---
+++ application/models/M_trxType.php
@@ -0,0 +1,166 @@
+<?php if (!defined('BASEPATH')) exit('No direct script allowed');
+
+/**
+ * @OA\Schema(schema="trxType")
+ */
+class M_trxType extends CI_Model
+{
+    public function tableName(): string
+    {
+        return "m_trx_type";
+    }
+    /**
+     * @OA\Property()
+     * @var int
+     */
+    public $id;
+    public function idField(): string
+    {
+        return "id";
+    }
+    public function idjsonKey(): string
+    {
+        return "id";
+    }
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $code;
+    public function codeField(): string
+    {
+        return "code";
+    }
+    public function codejsonKey(): string
+    {
+        return "code";
+    }
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $name;
+    public function nameField(): string
+    {
+        return "name";
+    }
+    public function nameJsonKey(): string
+    {
+        return "name";
+    }
+    /**
+     * @OA\Property()
+     * @var string
+     */
+    public $data;
+    public function dataField(): string
+    {
+        return "data";
+    }
+    public function dataJsonKey(): string
+    {
+        return "data";
+    }
+
+    function fromRow($row): M_trxType
+    {
+        $this->id = $row->id;
+        $this->code = $row->code;
+        $this->name = $row->name;
+        $this->data = $row->data;
+        return $this;
+    }
+
+    function fromJson($json): M_trxType
+    {
+        if (isset($json[$this->idjsonKey()])) {
+            $this->id = $json[$this->idjsonKey()];
+        }
+        if (isset($json[$this->codejsonKey()])) {
+            $this->code = $json[$this->codejsonKey()];
+        }
+        if (isset($json[$this->nameJsonKey()])) {
+            $this->name = $json[$this->nameJsonKey()];
+        }
+        if (isset($json[$this->dataJsonKey()])) {
+            $this->address = $json[$this->dataJsonKey()];
+        }
+        return $this;
+    }
+
+    public function fromId(String $id): M_trxType
+    {
+        $data = $this->db->get_where($this->tableName(), array('id' => $id));
+
+        return $this->fromRow($data->result()[0]);
+    }
+
+    function toArray(): array
+    {
+        $data = array(
+            $this->idField() => $this->id,
+            $this->codeField() => $this->code,
+            $this->nameField() => $this->name,
+            $this->dataField() => $this->data,
+        );
+
+        return $data;
+    }
+
+    function add(): M_trxType
+    {
+        try {
+            //generate key
+            $this->id = null;
+
+            $this->db->insert($this->tableName(), $this->toArray());
+
+            $data = $this->db->get_where($this->tableName(), array('code' => $this->code));
+
+            return $this->fromRow($data->result()[0]);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+
+    public function validateCode(): bool
+    {
+        $this->db->select('*');
+        $this->db->from($this->tableName());
+
+        $this->db->where($this->codeField(), $this->code);
+        $this->db->or_where($this->nameField(), $this->name);
+
+        $count = $this->db->count_all_results();
+
+        return $count > 0 ? false : true;
+    }
+
+    public function get(String $code = null, String $searchKey = null, int $limit = 0, int $skip = 10): array
+	{
+		$this->db->select('*');
+		$this->db->from($this->tableName());
+
+		if ($code != null && $code != "")
+			$this->db->where($this->idField(), $code);
+
+		if ($searchKey != null && $searchKey != "") {
+			$this->db->or_like($this->idField(), $searchKey);
+			$this->db->or_like($this->nameField(), $searchKey);
+		}
+
+		$this->db->order_by($this->idField(), "desc");
+		$this->db->limit($limit, $skip);
+
+		$query = $this->db->get();
+
+		$result = [];
+		foreach ($query->result() as $row) {
+			$member = new M_trxType();
+			$result[] = $member->fromRow($row);
+		}
+
+		return $result;
+	}
+}
