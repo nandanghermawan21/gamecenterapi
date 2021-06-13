62a544f292c2b79446165091851da5ea43d2a399
nandanghermawan <nandanghermawan21@GMAIL.COM>
2021-06-13 16:08:42 +0700

    test 9

--- application/controllers/api/TrxType.php
+++ application/controllers/api/TrxType.php
@@ -49,7 +49,7 @@ class TrxType extends BD_Controller
                 $trxType = $this->trxType->fromJson($jsonBody);
 
                 if ($trxType->validateCode() == true) {
-                    $this->response("Code Is Exist", 400);
+                    $this->response($jsonBody, 400);
                 } else {
                     $result = $this->trxType->fromJson($jsonBody)->add();
                     $this->response($result, 200);
